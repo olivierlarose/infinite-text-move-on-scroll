7a82f3fbb229ec513846dcb38292030c7b3061d0
Olivier Larose <olivierlarose1@gmail.com>
2023-05-28 00:33:59 -0400

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,59 @@
+'use client'
+import Image from 'next/image'
+import styles from './page.module.css'
+import { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+export default function Home() {
+
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect( () => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: true,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: e => direction = e.direction * -1
+      },
+      x: "-500px",
+    })
+    requestAnimationFrame(animate);
+  }, [])
+
+  const animate = () => {
+    if(xPercent < -100){
+      xPercent = 0;
+    }
+    else if(xPercent > 0){
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, {xPercent: xPercent})
+    gsap.set(secondText.current, {xPercent: xPercent})
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  }
+
+  return (
+    <main className={styles.main}>
+      <Image 
+        src="/images/background.jpg"
+        fill={true}
+        alt="background"
+      />
+      <div className={styles.sliderContainer}>
+        <div ref={slider} className={styles.slider}>
+          <p ref={firstText}>Freelance Developer -</p>
+          <p ref={secondText}>Freelance Developer -</p>
+        </div>
+      </div>
+    </main>
+  )
+}
